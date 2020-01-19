8e0a5818d937508f95a3bc218dc075fff9090608
FuriosoJack <iam@furiosojack.com>
2020-01-19 18:05:16 -0500

    Se cambio estilo de metatags

--- src/Http/Resources/PostResource.php
+++ src/Http/Resources/PostResource.php
@@ -60,25 +60,73 @@ class PostResource extends JsonResource
         $managetBlog = app('CodweltBlogManager');
         $myWith = [
             'meta_tags' => [
-                "description" => $this->resumen,
-                'keyworks' => $this->getMetaKeyWords(),
-                'robots' => Config::getRobotsArray()['post'],
-                'canonical' => $managetBlog->getUrlPostMetaTag($this->slug),
-                "article:author" => $this->creador->getNameModel(),
-                "article:published_time" => $this->created_at,
-                'og:title' => $this->titulo,
-                'og:description' => $this->resumen,
-                'og:url' =>  $managetBlog->getUrlPostMetaTag($this->slug),
-                'og:type' => 'article',
-                'og:locale' => app()->getLocale(),
-                'og:site_name' =>  $managetBlog->getDomainMetaTag(),
-                'og:image' => $this->getUrlMiniImage(),
-                //twitter
-                'twitter:title' =>$this->titulo,
-                'twitter:description' =>  $this->resumen,
-                'twitter:url' => $managetBlog->getUrlPostMetaTag($this->slug),
-                'twitter:site' => !empty(config('seotools.twitter.defaults.site'))  ? config('seotools.twitter.defaults.site') : '@codwelt',
-                'twitter:images0' => $this->getUrlMiniImage()
+                [
+                    "key" =>"description",
+                    "value" => $this->resumen
+                ],[
+                    "key" =>"keyworks",
+                    "value" => $this->getMetaKeyWords()
+                ],
+                [
+                    "key" =>"robots",
+                    "value" => Config::getRobotsArray()['post']
+                ],
+                [
+                    "key" =>"canonical",
+                    "value" => $managetBlog->getUrlPostMetaTag($this->slug)
+                ],
+                [
+                    "key" =>"article:author",
+                    "value" => $this->creador->getNameModel()
+                ],
+                [
+                    "key" =>"article:published_time",
+                    "value" => $this->created_at
+                ],
+                [
+                    "key" =>"og:description",
+                    "value" => $this->resumen
+                ],
+                [
+                    "key" =>"og:url",
+                    "value" => $managetBlog->getUrlPostMetaTag($this->slug)
+                ],
+                [
+                    "key" =>"og:type",
+                    "value" => "article"
+                ],
+                [
+                    "key" =>"og:locale",
+                    "value" => app()->getLocale()
+                ],
+                [
+                    "key" =>"og:site_name",
+                    "value" => $managetBlog->getDomainMetaTag()
+                ],
+                [
+                    "key" =>"og:image",
+                    "value" => $this->getUrlMiniImage()
+                ],
+                [
+                    "key" =>"twitter:title",
+                    "value" => $this->titulo
+                ],
+                [
+                    "key" =>"twitter:description",
+                    "value" => $this->resumen
+                ],
+                [
+                    "key" =>"twitter:url",
+                    "value" => $managetBlog->getUrlPostMetaTag($this->slug)
+                ],
+                [
+                    "key" =>"twitter:site",
+                    "value" => !empty(config('seotools.twitter.defaults.site'))  ? config('seotools.twitter.defaults.site') : '@codwelt',
+                ],
+                [
+                    "key" =>"twitter:images0",
+                    "value" => $this->getUrlMiniImage(),
+                ]
             ]
         ];
 
